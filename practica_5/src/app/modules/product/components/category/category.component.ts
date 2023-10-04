8c105fc15f3480a3fa1d2b77efe5ce7a41295aba
Raul_EMartinez <raulmartinez@ciencias.unam.mx>
2023-10-04 10:59:52 -0600

    Práctica finalizada.

---
+++ practica_5/src/app/modules/product/components/category/category.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { Category } from '../../_models/category';
+
+declare var $: any;
+
+@Component({
+  selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrls: ['./category.component.css']
+})
+
+export class CategoryComponent {
+  categories: Category[] = [];
+
+  constructor(){}
+
+  ngOnInit(){
+    this.getCategories();
+  }
+
+  getCategories(){
+    let category1 = new Category(1, 190406, "videojuego", "agotado");
+    let category2 = new Category(2, 311020, "videojuego", "rentado");
+    let category3 = new Category(3, 190972, "serie", "agotado");
+
+    this.categories.push(category1);
+    this.categories.push(category2);
+    this.categories.push(category3);
+  }
+}
