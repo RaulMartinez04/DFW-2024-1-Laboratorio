3e0b10c576d4b65f8f4cc571b5f7b4472edbfeb5
rubenAcostaArzate <rubenacostaarzate@ciencias.unam.mx>
2023-10-07 17:01:49 -0600

    Agregando importaciones de forms

--- practica_5/src/app/modules/product/_models/category.ts
+++ practica_5/src/app/modules/product/_models/category.ts
@@ -1,8 +1,12 @@
+import { FormBuilder } from "@angular/forms";
+
 export class Category{
     category_id: number = 0;
     code: number = 0;
     category: string = "";
     status: string = "";
+
+    private formBuilder: FormBuilder;
 
     constructor(category_id: number, code: number, category: string, status: string){
         this.category_id = category_id;
@@ -10,4 +14,8 @@ export class Category{
         this.category = category;
         this.status = status;
     }
+
+    
 }
+
+declare var $: any;
--- practica_5/src/app/modules/product/product.module.ts
+++ practica_5/src/app/modules/product/product.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoryComponent } from './components/category/category.component';
-
-
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms'; 
 
 @NgModule({
   declarations: [
