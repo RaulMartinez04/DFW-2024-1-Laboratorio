e2fddb043248581f198ce79e6b9bf717747148a8
Raul_EMartinez <raulmartinez@ciencias.unam.mx>
2023-10-17 23:43:24 -0600

    Práctica finalizada

--- practica_5/src/app/modules/product/components/category/category.component.ts
+++ practica_5/src/app/modules/product/components/category/category.component.ts
@@ -12,6 +12,7 @@ declare var $: any;
 
 export class CategoryComponent {
   categories: Category[] = [];
+  categoryUpdated: number = 0;
 
   form = this.formBuilder.group({
     category: ["", [Validators.required]],
@@ -28,25 +29,56 @@ export class CategoryComponent {
     this.getCategories();
   }
 
+  disableCategory(id: number){
+    for(let category of this.categories){
+      if(category.category_id == id){
+        category.status = 0;
+        alert("Región desactivada exitosamente!");
+        break;
+      }
+    }
+    console.log("SALIR")
+  }
+
+  enableCategory(id: number){
+    for(let category of this.categories){
+      if(category.category_id == id){
+        category.status = 1;
+        alert("Región activada exitosamente!");
+        break;
+      }
+    }
+  }
+
   getCategories(){
-    let category1 = new Category(1, "190406", "videojuego", "agotado");
-    let category2 = new Category(2, "311020", "videojuego", "rentado");
-    let category3 = new Category(3, "190972", "serie", "agotado");
+    let category1 = new Category(1, "190406", "videojuego", 0);
+    let category2 = new Category(2, "311020", "videojuego", 1);
+    let category3 = new Category(3, "190972", "serie", 0);
 
     this.categories.push(category1);
     this.categories.push(category2);
     this.categories.push(category3);
   }
 
+
   onSubmit(){
+    if(this.categoryUpdated == 0){
+      this.onSubmitCreate();
+    }else{
+      this.onSubmitUpdate();
+    }
+  }
+
+  onSubmitCreate(){
     this.submitted = true;
 
     if(this.form.invalid) return;
 
     this.submitted = false;
 
-    let categoria = new Category(0, this.form.controls['code'].value!, this.form.controls['category'].value!, "Aprobado");
-    this.categories.push(categoria);
+    let category = new Category(0, this.form.controls['category'].value!, this.form.controls['code'].value!, 1);
+    console.log(this.form.value);
+    this.categories.push(category);
     
     $("#modalForm").modal("hide");
 
@@ -54,10 +86,45 @@ export class CategoryComponent {
 
   }
 
+  onSubmitUpdate(){
+    this.submitted = true;
+
+    if(this.form.invalid) return;
+
+    this.submitted = false;
+
+    for(let category of this.categories){
+      if(category.category_id == this.categoryUpdated){
+        category.category = this.form.controls['category'].value!;
+        category.code = this.form.controls['code'].value!;
+        break;
+      }
+    }
+    
+    $("#modalForm").modal("hide");
+
+    alert("Región actualizada exitosamente!");
+
+    this.categoryUpdated = 0;
+
+  }
+
+  updateCategory(category: Category){
+    this.categoryUpdated = category.category_id;
+    
+    this.form.reset();
+    this.form.controls['category'].setValue(category.category);
+    this.form.controls['code'].setValue(category.code);
+    
+    this.submitted = false;
+    $("#modalForm").modal("show");
+  }
+
   // modals 
 
   showModalForm(){
     this.form.reset();
+    this.categoryUpdated = 0;
     this.submitted = false;
     $("#modalForm").modal("show");
   }
