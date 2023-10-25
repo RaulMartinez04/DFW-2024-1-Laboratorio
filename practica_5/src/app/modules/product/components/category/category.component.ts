728093a95808dbc215a1ebb66e40a257f2f7f2f8
Raul_EMartinez <raulmartinez@ciencias.unam.mx>
2023-10-25 10:52:10 -0600

    Práctica finaliza con error en el botón Nueva Categoría.

--- practica_5/src/app/modules/product/components/category/category.component.ts
+++ practica_5/src/app/modules/product/components/category/category.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { Category } from '../../_models/category';
 import { FormBuilder, Validators } from '@angular/forms';
+import { CategoryService } from '../../_services/category.service';
+
+import Swal from'sweetalert2'; // sweetalert
 
 declare var $: any;
 
@@ -14,54 +17,115 @@ export class CategoryComponent {
   categories: Category[] = [];
   categoryUpdated: number = 0;
 
+  // Formulario de registro.
   form = this.formBuilder.group({
     category: ["", [Validators.required]],
     code: ["", [Validators.required]],
   });
 
-  submitted = false;
+  submitted = false; // Indica si se envió el formulario.
 
   constructor(
     private formBuilder: FormBuilder,
+    private categoryService: CategoryService
   ){}
 
   ngOnInit(){
     this.getCategories();
   }
 
+  // CRUD Category
+
   disableCategory(id: number){
-    for(let category of this.categories){
-      if(category.category_id == id){
-        category.status = 0;
-        alert("Categoria desactivada exitosamente!");
-        break;
+    this.categoryService.disableCategory(id).subscribe(
+      res => {
+        // muestra mensaje de confirmación
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          toast: true,
+          text: 'La región ha sido desactivada',
+          background: '#E8F8F8',
+          showConfirmButton: false,
+          timer: 2000
+        });
+
+        this.getCategories(); // consulta regiones con los cambios realizados
+      },
+      err => {
+        // muestra mensaje de error
+        Swal.fire({
+          position: 'top-end',
+          icon: 'error',
+          toast: true,
+          showConfirmButton: false,
+          text: err.error.message,
+          background: '#F8E8F8',
+          timer: 2000
+        });
       }
-    }
-    console.log("SALIR")
+    );
   }
 
   enableCategory(id: number){
-    for(let category of this.categories){
-      if(category.category_id == id){
-        category.status = 1;
-        alert("Categoria activada exitosamente!");
-        break;
+    this.categoryService.enableCategory(id).subscribe(
+      res => {
+        // muestra mensaje de confirmación
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          toast: true,
+          text: 'La región ha sido activada',
+          background: '#E8F8F8',
+          showConfirmButton: false,
+          timer: 2000
+        });
+
+        this.getCategories(); // consulta regiones con los cambios realizados
+      },
+      err => {
+        // muestra mensaje de error
+        Swal.fire({
+          position: 'top-end',
+          icon: 'error',
+          toast: true,
+          showConfirmButton: false,
+          text: err.error.message,
+          background: '#F8E8F8',
+          timer: 2000
+        });
       }
-    }
+    );
   }
 
   getCategories(){
-    let category1 = new Category(1, "190406", "Videojuego", 0);
-    let category2 = new Category(2, "311020", "Videojuego", 1);
-    let category3 = new Category(3, "190972", "Serie", 0);
-
-    this.categories.push(category1);
-    this.categories.push(category2);
-    this.categories.push(category3);
+    this.categoryService.getCategories().subscribe(
+      res => {
+        this.categories = res; // asigna la respuesta de la API a la lista de regiones
+      },
+      err => {
+        // muestra mensaje de error
+        Swal.fire({
+          position: 'top-end',
+          icon: 'error',
+          toast: true,
+          showConfirmButton: false,
+          text: err.error.message,
+          background: '#F8E8F8',
+          timer: 2000
+        });
+      }
+    );
   }
 
 
   onSubmit(){
+    // valida el formulario
+    this.submitted = true;
+    if(this.form.invalid) return;
+    this.submitted = false;
+
+    // ejecuta la función crear o actualizar según corresponda
     if(this.categoryUpdated == 0){
       this.onSubmitCreate();
     }else{
@@ -70,43 +134,71 @@ export class CategoryComponent {
   }
 
   onSubmitCreate(){
-    this.submitted = true;
+    this.categoryService.createCategory(this.form.value).subscribe(
+      res => {
+        // muestra mensaje de confirmación
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          toast: true,
+          text: 'La categoría ha sido registrada',
+          background: '#E8F8F8',
+          showConfirmButton: false,
+          timer: 2000
+        });
 
-    if(this.form.invalid) return;
-
-    this.submitted = false;
-
-    let category = new Category(0, this.form.controls['code'].value!, this.form.controls['category'].value!, 1);
-    console.log(this.form.value);
-    this.categories.push(category);
+        this.getCategories(); // consulta regiones con los cambios realizados
     
-    $("#modalForm").modal("hide");
-
-    alert("Categoria guardada exitosamente!");
-
+        $("#modalForm").modal("hide"); // oculta el modal de registro
+      },
+      err => {
+        // muestra mensaje de error
+        Swal.fire({
+          position: 'top-end',
+          icon: 'error',
+          toast: true,
+          showConfirmButton: false,
+          text: err.error.message,
+          background: '#F8E8F8',
+          timer: 2000
+        });
+      }
+    );
   }
 
   onSubmitUpdate(){
-    this.submitted = true;
+    this.categoryService.updateCategory(this.form.value, this.categoryUpdated).subscribe(
+      res => {
+        // muestra mensaje de confirmación
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          toast: true,
+          text: 'La categoría ha sido actualizada',
+          background: '#E8F8F8',
+          showConfirmButton: false,
+          timer: 2000
+        });
 
-    if(this.form.invalid) return;
-
-    this.submitted = false;
-
-    for(let category of this.categories){
-      if(category.category_id == this.categoryUpdated){
-        category.category = this.form.controls['category'].value!;
-        category.code = this.form.controls['code'].value!;
-        break;
-      }
-    }
+        this.getCategories(); // consulta regiones con los cambios realizados
     
-    $("#modalForm").modal("hide");
+        $("#modalForm").modal("hide"); // oculta el modal de registro
 
-    alert("Categoria actualizada exitosamente!");
-
-    this.categoryUpdated = 0;
-
+        this.categoryUpdated = 0; // resetea el id de la región que se actualiza a 0
+      },
+      err => {
+        // muestra mensaje de error
+        Swal.fire({
+          position: 'top-end',
+          icon: 'error',
+          toast: true,
+          showConfirmButton: false,
+          text: err.error.message,
+          background: '#F8E8F8',
+          timer: 2000
+        });
+      }
+    );
   }
 
   updateCategory(category: Category){
